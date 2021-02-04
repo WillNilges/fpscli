95689aa67b12a49a81f66cc3510f3cc5d35f8bdd
Alphactory <aedan.e.taylor@gmail.com>
2021-02-04 02:09:51 -0500

    basic network code minus filewriting

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,21 @@
 #include <time.h>
 #include <vector>
 
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
 using namespace BitBorn;
 
 const std::chrono::duration<int, std::milli> framePeriod = std::chrono::milliseconds(1000 / 30);
 
 bool finished = false;
 bool showHUD = false;
+
+string serverIP = "localhost";
+int serverPort = 5080;
 
 int main() {
     std::string datadir = "./";
@@ -27,6 +36,42 @@ int main() {
     if (cDatadir != nullptr) {
         datadir = std::string(cDatadir);
     }
+    //Check with server to make sure map is current. If server not up, skip this step
+    try {
+        int sock = 0, valread;
+        struct sockaddr_in serv_addr;
+        char *msg = "map\n";
+        char buffer[1024] = {0};
+        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+            printf("\n Socket creation error \n");
+            return -1;
+        }
+
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(serverPort);
+
+        // Convert IPv4 and IPv6 addresses from text to binary form
+        if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+            printf("\nInvalid address/ Address not supported \n");
+            return -1;
+        }
+
+        if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+            printf("\nConnection Failed \n");
+            return -1;
+        }
+        send(sock, msg, strlen(msg), 0);
+        printf("Hello message sent\n");
+        valread = read(sock, buffer, 1024);
+        printf("%s\n", buffer);
+        string stringmap = string(buffer, 1024);
+        map = stringmap;
+        //
+    }
+    catch (...) {
+
+    }
+
     Map map(datadir + "/map"); // Get the map from a file and instantiate a map object
 
     // Seed the RNG
